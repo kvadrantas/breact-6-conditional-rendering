f957c79884994b2be5abc78dc09420bb917005ed
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-10-25 21:50:07 +0300

    Added date field + responsive design

--- breact-lesson/src/App.jsx
+++ breact-lesson/src/App.jsx
@@ -21,6 +21,7 @@ function App() {
             farm: farm,
             animal: a,
             weight: 0,
+            feadingDate: '',
             executed
         });
         setAnimal(animalCopy);
@@ -59,7 +60,16 @@ function App() {
         animalCopy[ind].weight = weight;
         setAnimal(animalCopy);
         localStorage.setItem('animals',    JSON.stringify(animalCopy));
-        document.getElementById(id).value = '';
+        document.getElementById('weight' + id).value = '';
+    }
+
+    const addFeadingDate = (id, feadingDate) => {
+        const animalCopy = animal.slice();
+        const ind = animalCopy.findIndex(e => e.id === id);
+        animalCopy[ind].feadingDate = feadingDate;
+        setAnimal(animalCopy);
+        localStorage.setItem('animals',    JSON.stringify(animalCopy));
+        document.getElementById('date' + id).value = '';
     }
 
     useEffect(() => {
@@ -72,18 +82,47 @@ function App() {
 
     return(
         <>
-            <div className="stats-title">STATISTICS:</div>
-            <div className="stats">
-                <div className="stat-block">
-                    <div className="stat-val">Cows: <span>{count('cow')}</span></div>
-                    <div className="stat-val">Sheeps: <span>{count('sheep')}</span></div>
-                    <div className="stat-val">Horses: <span>{count('horse')}</span></div>
+            <div className="top-pane">
+                <div>
+                    <div className="stats-title">STATISTICS:</div>
+                    <div className="stats">
+                        <div className="stat-block">
+                            <div className="stat-val">Cows: <span>{count('cow')}</span></div>
+                            <div className="stat-val">Sheeps: <span>{count('sheep')}</span></div>
+                            <div className="stat-val">Horses: <span>{count('horse')}</span></div>
+                        </div>
+                        <div className="stat-block">
+                            <div className="stat-val">Total animals: <span>{count('all')}</span></div>
+                            <div className="stat-val">Total weight: <span>{count('weight')}</span></div>
+                            <div className="stat-val">Executed: <span>{executed}</span></div>
+                        </div>
+                    </div>
                 </div>
-                <div className="stat-block">
-                    <div className="stat-val">Total animals: <span>{count('all')}</span></div>
-                    <div className="stat-val">Total weight: <span>{count('weight')}</span></div>
-                    <div className="stat-val">Executed: <span>{executed}</span></div>
-                </div>
+                    <div className="navigation">
+                        <fieldset>
+                            <legend>Add</legend>
+                            <div className="field-buttons">
+                            <select value={farm} onChange={pickFarm}>
+                                <option value={1}>Farm 1</option>
+                                <option value={2}>Farm 2</option>
+                                <option value={3}>Farm 3</option>
+                            </select>
+                        </div>
+                            <div className="animal-buttons">
+                            <button onClick={() => addAnimal('cow')}>Cow</button>
+                            <button onClick={() => addAnimal('sheep')}>Sheep</button>
+                            <button onClick={() => addAnimal('horse')}>Horse</button>
+                        </div>
+                        </fieldset>
+                        <fieldset>
+                            <legend>Remove</legend>
+                            <div className="animal-buttons">
+                            <button onClick={() => removeAnimals('cow')}>All cows</button>
+                            <button onClick={() => removeAnimals('sheep')}>All sheeps</button>
+                            <button onClick={() => removeAnimals('horse')}>All horses</button>
+                        </div>
+                        </fieldset>
+                    </div>
             </div>
 
 
@@ -92,7 +131,7 @@ function App() {
                 <div className='field'>
                     {animal.map((e, ind) => 
                         {if(e.farm === 1) 
-                            return(<Animals key={ind} animal={e.animal} id={e.id} weight={e.weight} removeAnimal={removeAnimal} addWeight={addWeight}/>);
+                            return(<Animals key={ind} animal={e.animal} id={e.id} weight={e.weight} feadingDate={e.feadingDate} removeAnimal={removeAnimal} addWeight={addWeight} addFeadingDate={addFeadingDate}/>);
                             return '';
                         } 
                     )}
@@ -100,7 +139,7 @@ function App() {
                 <div className="field">
                     {animal.map((e, ind) => 
                         {if(e.farm === 2) 
-                            return(<Animals key={ind} animal={e.animal} id={e.id} weight={e.weight} removeAnimal={removeAnimal} addWeight={addWeight}/>);
+                            return(<Animals key={ind} animal={e.animal} id={e.id} weight={e.weight} feadingDate={e.feadingDate} removeAnimal={removeAnimal} addWeight={addWeight} addFeadingDate={addFeadingDate}/>);
                             return '';
                         } 
                     )}
@@ -108,39 +147,10 @@ function App() {
                 <div className="field">
                     {animal.map((e, ind) => 
                         {if(e.farm === 3) 
-                            return(<Animals key={ind} animal={e.animal} id={e.id} weight={e.weight} removeAnimal={removeAnimal} addWeight={addWeight}/>);
+                            return(<Animals key={ind} animal={e.animal} id={e.id} weight={e.weight} feadingDate={e.feadingDate} removeAnimal={removeAnimal} addWeight={addWeight} addFeadingDate={addFeadingDate}/>);
                             return '';
                         } 
                     )}
-                </div>
-
-
-                <div className="navigation">
-
-                    <fieldset>
-                        <legend>Add</legend>
-                        <div className="field-buttons">
-                        <select value={farm} onChange={pickFarm}>
-                            <option value={1}>Farm 1</option>
-                            <option value={2}>Farm 2</option>
-                            <option value={3}>Farm 3</option>
-                        </select>
-                    </div>
-                        <div className="animal-buttons">
-                        <button onClick={() => addAnimal('cow')}>Cow</button>
-                        <button onClick={() => addAnimal('sheep')}>Sheep</button>
-                        <button onClick={() => addAnimal('horse')}>Horse</button>
-                    </div>
-                    </fieldset>
-                    <fieldset>
-                        <legend>Remove</legend>
-                        <div className="animal-buttons">
-                        <button onClick={() => removeAnimals('cow')}>All cows</button>
-                        <button onClick={() => removeAnimals('sheep')}>All sheeps</button>
-                        <button onClick={() => removeAnimals('horse')}>All horses</button>
-                    </div>
-                    </fieldset>
-                    
                 </div>
             </div>
         </>
